4fc1d0e7892b846a3303f135abfbd0559253d749
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-08 04:03:26 -0700

    changes in main for new conflict

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -2,7 +2,10 @@ public class FeatureA {
     public static void main(String[] args) {
 
         System.out.println("Hallo, das ist der Code von Florian");
+
+        // new im main
         System.out.println("jetzt brauchen wir den code von florian nicht mehr");
+
         System.out.println("Hallo, das ist der Code von Youmna");
     }
 }
